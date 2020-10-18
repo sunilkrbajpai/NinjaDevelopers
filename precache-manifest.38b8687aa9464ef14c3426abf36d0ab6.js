12520998d3a00e74283b219adca9dfe6b33b3714
Sunil kumar bajpai <sunilkumarbajpai.skb@gmail.com>
2020-10-19 00:40:40 +0530

    Updates

---
+++ precache-manifest.38b8687aa9464ef14c3426abf36d0ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc9a23c83c0f014ef6f0140204ffd50",
+    "url": "/NinjaDevelopers/index.html"
+  },
+  {
+    "revision": "33b3dc5d16a36f1238e2",
+    "url": "/NinjaDevelopers/static/js/2.bd18881a.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/NinjaDevelopers/static/js/2.bd18881a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89fd45dfcd3ea4792cb",
+    "url": "/NinjaDevelopers/static/js/main.6809d520.chunk.js"
+  },
+  {
+    "revision": "a0361ff2684c1d9145e7",
+    "url": "/NinjaDevelopers/static/js/runtime-main.934e69cf.js"
+  }
+]);
